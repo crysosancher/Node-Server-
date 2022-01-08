0f1d63042326f3f5235ad6446cb68bdcca50af5c
crysosancher <vibhupandey.vp@gmail.com>
2022-01-09 00:02:13 +0530

    add router

--- Express/app.js
+++ Express/app.js
@@ -2,20 +2,13 @@ const express=require('express')
 const bodyparser=require('body-parser')
 
 const app=express();
+const adminRoutes=require('./routes/admin')
+const shopRoutes=require('./routes/shop')
 
 app.use(bodyparser.urlencoded({extended: false}));
 
-app.use('/add-product',(req,res,next)=>{
-	res.send('<form action="/product" method="POST"><input type="text"name="title"><button type="submit">click on me</button></form>')
-})
-app.use('/product',(req,res,next)=>{
-	console.log(req.body)
-	res.redirect('/')
+app.use(adminRoutes)
+app.use(shopRoutes)
 
-})
-
-app.use('/',(req,res,next)=>{
-	res.send('<h1>Vibhu is everywhere</h1>')
-})
 
 app.listen(3000);
