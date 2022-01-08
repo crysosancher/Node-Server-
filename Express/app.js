09139d3901a7627438edd7e3d3753600bbd47db8
crysosancher <vibhupandey.vp@gmail.com>
2022-01-09 00:56:19 +0530

    error handeled page not found

--- Express/app.js
+++ Express/app.js
@@ -1,17 +1,21 @@
 const express=require('express')
 const bodyparser=require('body-parser')
+const path=require('path');
 
 const app=express();
+
 const adminRoutes=require('./routes/admin')
-const shopRoutes=require('./routes/shop')
+const shopRoutes=require('./routes/shop');
+
 
 app.use(bodyparser.urlencoded({extended: false}));
 
 app.use(adminRoutes)
 app.use(shopRoutes)
-app.use((req,res,next)=>{
-	res.status(404).send('<h1>Page not found</h1>')
-})
+app.use((req,res,next)=>{//target when any untarcked thing after / comes up
+	//res.status(404).sendFile(path.join(__dirname,"views","404.html"));
+	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 
 app.listen(3000);
