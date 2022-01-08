5b98fd430ed7b3633f000d0684bfb5393e497a95
crysosancher <vibhupandey.vp@gmail.com>
2022-01-08 21:09:00 +0530

    Middle ware

---
+++ Express/app.js
@@ -0,0 +1,14 @@
+const http=require('http');
+const express=require('express')
+
+const app=express();
+app.use((req,res,next)=>{
+	console.log("in the middleware")
+next();//Allows the req to continue to the next Middleware in line.
+});
+app.use((req,res,next)=>{
+	console.log("in the another middleware")
+
+})
+const server= http.createServer(app);
+server.listen(3000);
