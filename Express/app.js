f6fe4471677d90ae1dfe3c79eded35f8147162e3
crysosancher <vibhupandey.vp@gmail.com>
2022-01-08 21:39:10 +0530

    middleware sharp

--- Express/app.js
+++ Express/app.js
@@ -1,14 +1,19 @@
 const express=require('express')
 
 const app=express();
-app.use((req,res,next)=>{//everything has to pass through these middleware
-	console.log("in the middleware")
-next();//Allows the req to continue to the next Middleware in line.
-});
-app.use((req,res,next)=>{
+app.use('/',(req,res,next)=>{
+	console.log("I must run everytime")
+	next();//easily going to any next condition. Note->req moves from top to bottom
+
+})
+
+app.use('/message',(req,res,next)=>{
+	res.send('<h1>This what she said what Vibhu is everywhere</h1>')
+})
+
+app.use('/',(req,res,next)=>{
 	console.log("in the another middleware")
 	res.send('<h1>Vibhu is everywhere</h1>')
-
 })
 
 app.listen(3000);
