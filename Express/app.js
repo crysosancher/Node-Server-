b80eac8c912665997a35eb6bed6ca5ed729fd46b
crysosancher <vibhupandey.vp@gmail.com>
2022-01-08 21:22:17 +0530

    cleaner

--- Express/app.js
+++ Express/app.js
@@ -1,14 +1,14 @@
-const http=require('http');
 const express=require('express')
 
 const app=express();
-app.use((req,res,next)=>{
+app.use((req,res,next)=>{//everything has to pass through these middleware
 	console.log("in the middleware")
 next();//Allows the req to continue to the next Middleware in line.
 });
 app.use((req,res,next)=>{
 	console.log("in the another middleware")
+	res.send('<h1>Vibhu is everywhere</h1>')
 
 })
-const server= http.createServer(app);
-server.listen(3000);
+
+app.listen(3000);
