9a1bd9fdfdfc5577bebc603a15256ec57700a5c4
crysosancher <vibhupandey.vp@gmail.com>
2022-01-12 14:59:39 +0530

    Added errror page

--- Express/app.js
+++ Express/app.js
@@ -1,21 +1,20 @@
-const express=require('express')
-const bodyparser=require('body-parser')
-const path=require('path');
+const path = require('path');
 
-const app=express();
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const adminRoutes=require('./routes/admin')
-const shopRoutes=require('./routes/shop');
+const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes)
-app.use(shopRoutes)
-app.use((req,res,next)=>{//target when any untarcked thing after / comes up
-	//res.status(404).sendFile(path.join(__dirname,"views","404.html"));
-	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
-
 
 app.listen(3000);
--- Express/routes/admin.js
+++ Express/routes/admin.js
@@ -1,16 +1,18 @@
-const express=require('express');
-const path=require('path')
+const path = require('path');
 
-const router=express.Router();
+const express = require('express');
 
-router.get('/add-product',(req,res,next)=>{
-	res.sendFile(path.join(__dirname,"../","views","add-product.html"))
-})
+const router = express.Router();
 
-router.post('/product',(req,res,next)=>{
-	console.log(req.body)
-	res.redirect('/')
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+});
 
-})
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
 
 module.exports = router;
